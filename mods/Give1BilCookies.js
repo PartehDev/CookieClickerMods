3ee123f2d705d60754b89af4943bf7ca0ffd99f1
PartehDev <89151213+PartehDev@users.noreply.github.com>
2022-05-21 15:23:23 -0500

    Update Give1BilCookies.js

--- mods/Give1BilCookies.js
+++ mods/Give1BilCookies.js
@@ -1,3 +1,3 @@
 function exec() { Game.Earn(1000000000) }
 Game.Notify("1BCookies Giver Mod","Every 5 seconds, you get 1b cookies >:)")
-window.setInterval( function () { exec() }, 9999 )
+window.setInterval( function () { exec() }, 4999 )
