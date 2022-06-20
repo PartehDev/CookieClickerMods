07db62a7b0892c378070b0f8dc09d0dac90d859b
Parteh the Coder <89151213+PartehDev@users.noreply.github.com>
2022-06-20 18:17:18 -0500

    Update OPMod.js

--- mods/OPMod.js
+++ mods/OPMod.js
@@ -1,4 +1,4 @@
 let delay = 1000 * 60 * 60
 function exec() { Game.Earn(1000000000000000000)}
-Game.Notify("OPMod Activated"," You will get 1 QUINTILLION cookies every hour")
+Game.Notify("OPMod Activated","You will get 1 QUINTILLION cookies every hour")
 window.setInterval(function () {exec()}, delay)
